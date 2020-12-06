c62cfc4cc29d424f1d2136f8683b19850a4df1c9
Natalia Martian <natalia.martian@gmail.com>
2020-12-06 00:20:34 -0500

    Set up for react refactoring

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const message = "I'm a message"
+  const buttonStyle = {fontSize:"4em", backgroundColor:"red"}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{message}</p>
+      <button style={buttonStyle}>I am button</button>
     </div>
   );
 }
